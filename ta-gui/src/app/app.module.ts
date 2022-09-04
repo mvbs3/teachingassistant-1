6aa01e518fdb04b572a088b294554df6d614012d
mvbs3 <mvbs3@cin.ufpe.br>
2022-09-04 18:56:35 -0300

    resolvendo unmerge?

--- ta-gui/src/app/app.module.ts
+++ ta-gui/src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
+<<<<<<< HEAD
 import { BrowserModule } from '@angular/platform-browser';
+=======
+import { FormsModule } from '@angular/forms';
+>>>>>>> dbc0756 (elementos do formulario para cadastro de alunos)
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +12,13 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+<<<<<<< HEAD
     BrowserModule
+=======
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+>>>>>>> dbc0756 (elementos do formulario para cadastro de alunos)
   ],
   providers: [],
   bootstrap: [AppComponent]
